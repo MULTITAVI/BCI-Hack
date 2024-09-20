9eb0b344925527bc6860e1814482c873ad68e751
multitavi <multitavi@mail.ru>
2024-09-20 11:32:51 +0300

    DC_motor and encoder classes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/multitavi/ros/src
+# Build directory: /home/multitavi/ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("abot_driver")
---
+++ build/abot_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/multitavi/ros/src/abot_driver
+# Build directory: /home/multitavi/ros/build/abot_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
